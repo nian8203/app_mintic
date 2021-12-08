e20123fbfea1b7a99863e8cc53e85f27714aa10a
nian8203 <nelsonrodriguez@misena.edu.co>
2021-12-08 17:46:17 -0500

    Creación app

---
+++ app/src/main/java/com/reto1_2/appmintic/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.reto1_2.appmintic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText et_user, et_pass;
+    TextView tv_lost_pass, tv_registro;
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        et_user = findViewById(R.id.et_user);
+        et_pass = findViewById(R.id.et_pass);
+        tv_lost_pass = findViewById(R.id.tv_lost_pass);
+        tv_registro = findViewById(R.id.tv_registro);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+        tv_lost_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this, LostPassActivity.class));
+            }
+        });
+
+        tv_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this ,RegistroActivity.class));
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/reto1_2/appmintic/LostPassActivity.java
@@ -0,0 +1,14 @@
+package com.reto1_2.appmintic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LostPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_pass);
+    }
+}
